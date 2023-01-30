839fb6eee90be7d9461b8e0148324324cc2f36e9
antonionicolau <antonio@keleya.de>
2023-01-30 19:05:32 +0100

    added MyApp and useful comments

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:overlay_all_app/src/home_page.dart';
 import 'package:overlay_all_app/src/overlay_screen.dart';
 
+// Need to have it global to be able to access it everywhere
 OverlayScreen? overlayScreen;
 
 void main() {
   runApp(
-    MaterialApp(
+    const MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: const HomePage(),
       builder: (context, child) {
         return Overlay(
@@ -20,6 +30,6 @@ void main() {
           ],
         );
       },
-    ),
-  );
+    );
+  }
 }
